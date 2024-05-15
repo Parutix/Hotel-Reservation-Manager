0450e3f33d15c5d27e0865aaf57571a7d48a9881
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-05-15 18:58:42 +0300

    created base for frontend

---
+++ FRONTEND/frontend/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="search_container">
+        <div className="title">Hotel Reservation Manager</div>
+        <input className="search_input"></input>
+        <button className="search_button">Search</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
