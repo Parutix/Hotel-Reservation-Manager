a272f6852cd361a8cf5fb42cd7a19440d1827ad5
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-05-16 00:16:23 +0300

    get location on frontend

--- FRONTEND/frontend/src/App.js
+++ FRONTEND/frontend/src/App.js
@@ -1,6 +1,21 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, []);
+
+  const printLocation = () => {
+    console.log(latitude, longitude);
+  };
+
   return (
     <div className="App">
       <div className="search_container">
@@ -9,7 +24,9 @@ function App() {
           className="search_input"
           placeholder="Input the Radius in Kilometers"
         ></input>
-        <button className="search_button">Search</button>
+        <button className="search_button" onClick={printLocation}>
+          Search
+        </button>
       </div>
       <div className="search_results">
         <div className="result_container">
