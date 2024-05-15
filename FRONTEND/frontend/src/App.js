97c35a3f369304d778646de9d1c16932ce70db0d
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2024-05-15 23:36:45 +0300

    created container for results

--- FRONTEND/frontend/src/App.js
+++ FRONTEND/frontend/src/App.js
@@ -5,8 +5,16 @@ function App() {
     <div className="App">
       <div className="search_container">
         <div className="title">Hotel Reservation Manager</div>
-        <input className="search_input"></input>
+        <input
+          className="search_input"
+          placeholder="Input the Radius in Kilometers"
+        ></input>
         <button className="search_button">Search</button>
+      </div>
+      <div className="search_results">
+        <div className="result_container">
+          Search results will appear here..
+        </div>
       </div>
     </div>
   );
